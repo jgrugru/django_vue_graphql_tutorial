8eaed74bda48f695a9ad506bdce80fe4d1821bc6
jgrugru <jgruenbaum@captionpartners.com>
2021-11-10 16:29:41 -0600

    first commit

---
+++ dvg/blog/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from blog.models import Profile, Post, Tag
+# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    model = Profile
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    model = Tag
